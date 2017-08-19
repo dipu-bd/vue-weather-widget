7d24d39932e4d873663d48369dd2318695ffde1e
Sudipto Chandra <dipu.sudipta@gmail.com>
2017-08-20 02:37:38 +0600

    Add required props

--- src/components/vue-weather-widget.vue
+++ src/components/vue-weather-widget.vue
@@ -5,8 +5,35 @@
 </template>
 
 <script>
+import Helper from '../helper';
+
 export default {
-	name: 'vue-weather-widget'
+	name: 'vue-weather-widget',
+
+	/* details at https://darksky.net/dev/docs/forecast */
+	props: {
+		// Your Dark Sky secret key
+		key: {
+			type: String,
+			required: true
+		},
+
+		// The latitude of a location (in decimal degrees). 
+		// Positive is north, negative is south.
+		latitude: {
+			type: String,
+			required: true
+		},
+
+		// The longitude of a location (in decimal degrees).
+		// Positive is east, negative is west.
+		longitude: {
+			type: String,
+			required: true
+		},
+
+		
+	},
 }
 </script>
 
