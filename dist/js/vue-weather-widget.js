9a0d0549dd88e95c1cec2c88e748c0409f442527
Sudipto Chandra <dipu.sudipta@gmail.com>
2017-08-20 02:19:05 +0600

    Build app

---
+++ dist/js/vue-weather-widget.js
@@ -0,0 +1,15 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.VuePopper = factory());
+}(this, (function () { 'use strict';
+
+var VueWeatherWidget$1 = { render: function render() {
+		var _vm = this;var _h = _vm.$createElement;var _c = _vm._self._c || _h;return _c('div', [_vm._v("Vue weather widget")]);
+	}, staticRenderFns: [], _scopeId: 'data-v-2dd75d74',
+	name: 'vue-weather-widget'
+};
+
+return VueWeatherWidget$1;
+
+})));
