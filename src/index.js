3aa7e4c750888dc1082869e7d6c657dbb809b205
dipu-bd <dipu.sudipta@gmail.com>
2017-12-10 08:20:14 +0600

    Require jquery and css style in index if available

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import VueWeatherWidget from './components/vue-weather-widget.vue';
+if(window && require && !(window.$ || window.jQuery)) {
+	window.$ = window.jQuery = require('jquery');
+	require('./dist/css/vue-weather-widget.css');
+}
 
+import VueWeatherWidget from './components/vue-weather-widget.vue';
 export default VueWeatherWidget;
