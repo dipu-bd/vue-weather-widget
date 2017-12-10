7d06803a7dabd51005d47cc622c2ea370994ec2e
dipu-bd <dipu.sudipta@gmail.com>
2017-12-10 08:06:42 +0600

    Minify css files

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,29 +2,41 @@ import fs from 'fs';
 import vue from 'rollup-plugin-vue';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
+import css from 'rollup-plugin-css-porter';
+
+const production = (process.env.NODE_ENV === 'production');
 
 export default {
-  entry: 'src/index.js',
-  dest: process.env.NODE_ENV === 'production' ? 'dist/js/vue-weather-widget.min.js' : 'dist/js/vue-weather-widget.js',
-  moduleName: 'VueWeatherWidget',
-  format: 'umd',
-  plugins: [
-    vue({
-      css (style, styles, compiler) {
-        fs.writeFileSync('dist/css/vue-weather-widget.css', style);
-      }
-    }),
-    babel({
-      babelrc: false,
-      runtimeHelpers: true,
-      externalHelpers: false,
-      exclude: 'node_modules/**',
-      presets: [['es2015', {'modules': false}]],
-      plugins: [
-        'transform-object-assign',
-        'external-helpers'
-      ]
-    }),
-    (process.env.NODE_ENV === 'production' && uglify())
-  ],
+	entry: 'src/index.js',
+	dest: production ? 'dist/weather-widget.min.js' : 'dist/weather-widget.js',
+	moduleName: 'VueWeatherWidget',
+	format: 'umd',
+	plugins: [
+		css({
+			minified: production
+		}),
+		vue({
+			css (style) {
+				if(!production) {
+					fs.writeFileSync('dist/weather-widget.css', style.trim());
+				} else {
+					var minified = style.replace(/\s/g, ' ').trim();
+					minified = minified.replace(/\s\s/g, ' ');
+					fs.writeFileSync('dist/weather-widget.min.css', minified);
+				}
+			}
+		}),
+		babel({
+			babelrc: false,
+			runtimeHelpers: true,
+			externalHelpers: false,
+			exclude: 'node_modules/**',
+			presets: [['es2015', {'modules': false}]],
+			plugins: [
+				'transform-object-assign',
+				'external-helpers'
+			]
+		}),
+		(production && uglify())
+	],
 };
